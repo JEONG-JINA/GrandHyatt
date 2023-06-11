485d7a79ccc31621682769499958413ac109c837
jina Jeong <jina5389@gmail.com>
2023-06-12 06:52:59 +0900

    스크립트 수정

--- js/main.js
+++ js/main.js
@@ -50,9 +50,9 @@ fcTit.forEach(function(item, index) {
     });
 });
 */
-sec_fc();
 $(function() {
-    function sec_fc() {
+    $(".section-fc .fc_tit .titWrap").click(function() {
+        var i = $(this).index();
         var fcTitItem = $(".section-fc .fc_tit .titWrap");
         
         fcTitItem.each(function(clickedItem) {
@@ -67,10 +67,5 @@ $(function() {
         $(".section-fc .fc_tit .titWrap").not($(".section-fc .fc_tit .titWrap").eq(i)).removeClass("on");
         $(".section-fc .content .cont").eq(i).stop().fadeIn();
         $(".section-fc .content .cont").not($(".section-fc .content .cont").eq(i)).stop().fadeOut();
-    }
-    
-    $(".section-fc .fc_tit .titWrap").click(function() {
-        var i = $(this).index();
-        sec_fc();
     });
 });
